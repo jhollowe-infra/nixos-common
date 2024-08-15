1285a9503bf155e93bbccdff6ba4ec70a1efa490
John Hollowell <jhollowe@johnhollowell.com>
2024-08-14 22:53:40 -0400

    Adjust swappiness

--- fs/swapfile.nix
+++ fs/swapfile.nix
@@ -7,6 +7,7 @@
         size = lib.mkDefault 1024; # in MB
       }
     ];
+    # really try to not swap
     boot.kernel.sysctl."vm.swappiness" = lib.mkDefault 1;
   };
 }
--- fs/zram_swap.nix
+++ fs/zram_swap.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ lib, ... }:
 {
-  config.zramSwap = {
+  config = {
+  zramSwap = {
     enable = true;
     algorithm = "zstd";
+    # percent of memory to dedicate to zram
+    memoryPercent = lib.mkDefault 70; # TODO find a way to reduce this if the system already has a lot of memory
+  };
+  # fully lean into swapping (at a slightly higher override priority than ./swapfile.nix)
+  boot.kernel.sysctl={"vm.swappiness" = lib.mkOverride 999 150; };
   };
 }
