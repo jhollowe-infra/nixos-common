5a64ff89e68de9f089395a01b97733c293431658
John Hollowell <jhollowe@johnhollowell.com>
2024-08-15 00:09:14 -0400

    Add zram override module
    Allows a fully custom zram size value (defaults to 70% of memory up to 2GB)

--- fs/zram_swap.nix
+++ fs/zram_swap.nix
@@ -1,11 +1,13 @@
 { lib, ... }:
 {
+  imports = [ ../overrides/modules/zram.nix ];
+
   config = {
   zramSwap = {
     enable = true;
     algorithm = "zstd";
-    # percent of memory to dedicate to zram
-    memoryPercent = lib.mkDefault 70; # TODO find a way to reduce this if the system already has a lot of memory
+    # use 70% of ram (up to 2 GB) (seems to average ~ 3:1 compression)
+    memoryEquation = "min(70/100 * ram, 2048)";
   };
   # fully lean into swapping (at a slightly higher override priority than ./swapfile.nix)
   boot.kernel.sysctl={"vm.swappiness" = lib.mkOverride 999 150; };
